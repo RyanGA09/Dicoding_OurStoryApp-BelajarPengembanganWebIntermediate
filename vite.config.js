f9ab54edd72cb3878acc50a52422c7975e6f7dc9
Ryan <ryangadinga90@gmail.com>
2025-04-23 20:13:16 +0700

    update config vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   root: resolve(__dirname, "src"),
+  publicDir: resolve(__dirname, "public"),
   build: {
     outDir: resolve(__dirname, "dist"),
     emptyOutDir: true,
