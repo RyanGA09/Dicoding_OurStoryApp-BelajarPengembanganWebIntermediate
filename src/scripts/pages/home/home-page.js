0f5c586dbd66b9ce69269ba458dac4d9668d7ec8
Ryan <ryangadinga90@gmail.com>
2025-05-07 20:03:52 +0700

    update code

--- src/scripts/pages/home/home-page.js
+++ src/scripts/pages/home/home-page.js
@@ -89,6 +89,7 @@ import {
 } from "../../templates";
 import HomePresenter from "./home-presenter";
 import Map from "../../utils/map";
+import * as CityCareAPI from "../../data/api";
 
 export default class HomePage {
   #presenter = null;
@@ -104,34 +105,62 @@ export default class HomePage {
       </section>
 
       <section class="container">
-        <h1 class="section-title">Cerita Terbaru</h1>
-        <div id="story-list" class="story-list"></div>
+        <h1 class="section-title">Daftar Laporan Kerusakan</h1>
 
-        <!-- Peta dipisahkan sebagai elemen khusus -->
-        <div class="map-fixed-footer">
-          <div class="map-header">Peta Lokasi Cerita</div>
-          <div id="map"></div>
+        <div class="storys-list__container">
+          <div id="storys-list"></div>
+          <div id="storys-list-loading-container"></div>
         </div>
-
-        <h1 class="section-title">Cerita Terbaru</h1>
-        <div id="story-list" class="story-list"></div>
       </section>
     `;
   }
 
   async afterRender() {
-    this.#presenter = new HomePresenter(this);
-    await this.initialMap();
-    await this.#presenter.showStories();
+    this.#presenter = new HomePresenter({
+      view: this,
+      model: CityCareAPI,
+    });
+
+    await this.#presenter.initialGalleryAndMap();
   }
 
-  // async #setupMap() {
-  //   this.#map = L.map("map").setView([-6.2, 106.8], 5);
-  //   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  //     attribution:
-  //       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  //   }).addTo(this.#map);
-  // }
+  populatestorysList(message, storys) {
+    if (storys.length <= 0) {
+      this.populatestorysListEmpty();
+      return;
+    }
+
+    const html = storys.reduce((accumulator, story) => {
+      if (this.#map) {
+        const coordinate = [story.location.latitude, story.location.longitude];
+        const markerOptions = { alt: story.title };
+        const popupOptions = { content: story.title };
+        this.#map.addMarker(coordinate, markerOptions, popupOptions);
+      }
+
+      return accumulator.concat(
+        generateStoryItemTemplate({
+          ...story,
+          storyerName: story.storyer.name,
+        })
+      );
+    }, "");
+
+    document.getElementById("storys-list").innerHTML = `
+      <div class="storys-list">${html}</div>
+    `;
+  }
+
+  populateStorysListEmpty() {
+    document.getElementById("storys-list").innerHTML =
+      generateStorysListEmptyTemplate();
+  }
+
+  populatestorysListError(message) {
+    document.getElementById("storys-list").innerHTML =
+      generateStorysListErrorTemplate(message);
+  }
+
   async initialMap() {
     this.#map = await Map.build("#map", {
       zoom: 10,
@@ -139,43 +168,21 @@ export default class HomePage {
     });
   }
 
-  showStories(stories) {
-    const container = document.getElementById("story-list");
+  showMapLoading() {
+    document.getElementById("map-loading-container").innerHTML =
+      generateLoaderAbsoluteTemplate();
+  }
 
-    if (stories && stories.length > 0) {
-      container.innerHTML = stories
-        .map((story) => {
-          if (story.lat && story.lon) {
-            L.marker([story.lat, story.lon])
-              .addTo(this.#map)
-              .bindPopup(
-                `<b>${story.name}</b><br>${story.description.slice(0, 50)}...`
-              );
-          }
+  hideMapLoading() {
+    document.getElementById("map-loading-container").innerHTML = "";
+  }
 
-          return `
-          <div class="story-item">
-            <img src="${story.photoUrl}" alt="${story.name}" />
-            <h3>${story.name}</h3>
-            <p class="date">${new Date(story.createdAt).toLocaleDateString(
-              "id-ID"
-            )}</p>
-            <p>${story.description.slice(0, 100)}...</p>
-            ${
-              story.lat && story.lon
-                ? `
-              <a href="https://maps.google.com?q=${story.lat},${story.lon}" target="_blank" class="map-link">
-                Lihat Lokasi di Google Maps
-              </a>`
-                : ""
-            }
-            <a href="#/detail/${story.id}" class="btn-detail">Lihat Detail</a>
-          </div>
-        `;
-        })
-        .join("");
-    } else {
-      container.innerHTML = `<p>Belum ada cerita yang diposting.</p>`;
-    }
+  showLoading() {
+    document.getElementById("storys-list-loading-container").innerHTML =
+      generateLoaderAbsoluteTemplate();
+  }
+
+  hideLoading() {
+    document.getElementById("storys-list-loading-container").innerHTML = "";
   }
 }
--- src/scripts/utils/map.js
+++ src/scripts/utils/map.js
@@ -158,6 +158,10 @@ export default class Map {
     });
   }
 
+  /**
+   * Reference of using this static method:
+   * https://stackoverflow.com/questions/43431550/how-can-i-invoke-asynchronous-code-within-a-constructor
+   * */
   static async build(selector, options = {}) {
     if ("center" in options && options.center) {
       return new Map(selector, options);
@@ -165,6 +169,7 @@ export default class Map {
 
     const jakartaCoordinate = [-6.2, 106.816666];
 
+    // Using Geolocation API
     if ("locate" in options && options.locate) {
       try {
         const position = await Map.getCurrentPosition();
@@ -222,7 +227,7 @@ export default class Map {
 
   createIcon(options = {}) {
     return icon({
-      ...L.Icon.Default.prototype.options, // Perbaiki penggunaan L.Icon.Default
+      ...Icon.Default.prototype.options,
       iconRetinaUrl: markerIcon2x,
       iconUrl: markerIcon,
       shadowUrl: markerShadow,
